4291044b78775b4ff96afe5d3fe8bb0fd4bf6fef
Christian Skorobogatow <christiansko@icloud.com>
2022-08-20 17:27:31 +0200

    Initial Commit

---
+++ CryptoApp/CryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoAppApp.swift
+//  CryptoApp
+//
+//  Created by Christian Skorobogatow on 20/8/22.
+//
+
+import SwiftUI
+
+@main
+struct CryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
