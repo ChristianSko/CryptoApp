ad776ffc532c8e5567648e8902e27e5bc19b2f85
Christian Skorobogatow <christiansko@icloud.com>
2022-08-20 17:45:32 +0200

    feat(): project setup + color setup

--- CryptoApp/ContentView.swift
+++ CryptoApp/ContentView.swift
@@ -9,13 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack(spacing: 40) {
+                Text("Accent Color")
+                    .foregroundColor(.theme.accent)
+                
+                Text("Seconday Text Color")
+                    .foregroundColor(.theme.secondaryText)
+                
+                Text("Red Color")
+                    .foregroundColor(.theme.red)
+                
+                Text("Red Color")
+                    .foregroundColor(.theme.green)
+            }
+            .font(.headline)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .preferredColorScheme(.dark)
     }
 }
