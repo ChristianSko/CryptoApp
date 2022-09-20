104e41660975e61b82f473920e448c963726dbfe
Christian Skorobogatow <christiansko@icloud.com>
2022-09-20 23:53:59 +0200

    feat(): add logoview and searchbar to portfolioview

--- CryptoApp/Core/Home/Views/PortfolioView.swift
+++ CryptoApp/Core/Home/Views/PortfolioView.swift
@@ -10,12 +10,22 @@ import SwiftUI
 struct PortfolioView: View {
     
     @Environment(\.dismiss) var dismiss
+    @EnvironmentObject private var vm: HomeViewModel
     
     var body: some View {
         NavigationView {
             ScrollView {
                 VStack(alignment: .leading, spacing: 0) {
-                    Text("Hi")
+                    SearchBarView(searchText: $vm.searchText)
+
+                    ScrollView(.horizontal, showsIndicators: false) {
+                        LazyHStack(spacing: 10) {
+                            ForEach(vm.allCoins) { coin in
+                                CoinLogoView(coin: coin)
+                                    .frame(width: 75)
+                            }
+                        }
+                    }
                 }
             }
             .navigationTitle("Edit Profile")
@@ -31,5 +41,6 @@ struct PortfolioView: View {
 struct PortfolioView_Previews: PreviewProvider {
     static var previews: some View {
         PortfolioView()
+            .environmentObject(dev.homeVM)
     }
 }
