ddfa4077798e8dffbbf5544ff7871a9c61919db1
Christian Skorobogatow <christiansko@icloud.com>
2022-09-22 22:47:42 +0200

    feat(): finish portofolioview

--- CryptoApp/Core/Home/Views/PortfolioView.swift
+++ CryptoApp/Core/Home/Views/PortfolioView.swift
@@ -13,6 +13,7 @@ struct PortfolioView: View {
     @EnvironmentObject private var vm: HomeViewModel
     @State private var selectedCoin : CoinModel? = nil
     @State private var quantityText: String = ""
+    @State private var showCheckMark: Bool = false
     
     var body: some View {
         NavigationView {
@@ -23,31 +24,7 @@ struct PortfolioView: View {
                     coinLogoList
                     
                     if selectedCoin != nil {
-                        VStack(spacing: 20) {
-                            HStack {
-                                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
-                                
-                                Spacer()
-                                
-                                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
-                            }
-                            
-                            Divider()
-                            
-                            HStack {
-                                Text("Amount in your portfolio")
-                                Spacer()
-                                TextField("Ex. 1.4", text: $quantityText)
-                                    .multilineTextAlignment(.trailing)
-                                    .keyboardType(.decimalPad)
-                            }
-                            Divider()
-                            HStack {
-                                Text("Current value:")
-                                Spacer()
-                                Text("")
-                            }
-                        }
+                        portfolioInputSection
                     }
                 }
             }
@@ -55,6 +32,9 @@ struct PortfolioView: View {
             .toolbar {
                 ToolbarItem(placement: .navigationBarLeading) {
                     XMarkButton(dismiss: dismiss)
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    trailingNavBarButtons
                 }
             }
         }
@@ -88,9 +68,91 @@ extension PortfolioView {
                         )
                 }
             }
-            .padding(.vertical, 4)
+            .frame(height: 120)
             .padding(.leading)
         }
     }
+    
+    private var portfolioInputSection: some View {
+        VStack(spacing: 20) {
+            HStack {
+                Text("Current price of \(selectedCoin?.symbol.uppercased() ?? ""):")
+                
+                Spacer()
+                
+                Text(selectedCoin?.currentPrice.asCurrencyWith6Decimals() ?? "")
+            }
+            
+            Divider()
+            
+            HStack {
+                Text("Amount holding")
+                Spacer()
+                TextField("Ex. 1.4", text: $quantityText)
+                    .multilineTextAlignment(.trailing)
+                    .keyboardType(.decimalPad)
+            }
+            Divider()
+            HStack {
+                Text("Current value:")
+                Spacer()
+                Text(getCurrentValue().asCurrencyWith2Decimals())
+            }
+        }
+        .animation(.none)
+        .padding()
+        .font(.headline)
+    }
+    
+    private var trailingNavBarButtons: some View {
+        HStack(spacing: 10) {
+            Image(systemName: "checkmark")
+                .opacity(showCheckMark ? 1.0 : 0.0)
+            Button {
+                
+            } label: {
+                Text("Save".uppercased())
+                    .opacity(
+                        (selectedCoin != nil && selectedCoin?.currentHoldings != Double(quantityText)) ? 1.0 : 0.0
+                    )
+            }
+            .font(.headline)
+        }
+    }
+    
+    private func getCurrentValue() -> Double {
+        if let quantity = Double(quantityText) {
+            return quantity * (selectedCoin?.currentPrice ?? 0)
+        }
+        return 0
+    }
+    
+    private func saveButtonPressed() {
+        guard let coin = selectedCoin else { return }
+        
+        // save to portfolio
+        
+        
+        // show checkmark
+        withAnimation(.easeIn) {
+            showCheckMark = true
+            removeSelectedCoin()
+        }
+        
+        //hide keyboard
+        UIApplication.shared.endEditing()
+        
+        //hide checkmark
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            withAnimation(.easeOut) {
+                showCheckMark = false
+            }
+        }
+    }
+    
+    private func removeSelectedCoin() {
+        selectedCoin = nil
+    }
+    
     
 }
