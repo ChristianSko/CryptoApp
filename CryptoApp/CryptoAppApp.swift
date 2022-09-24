3a32a098bd8f9e2ed056205b8bc5ec002be290bb
Christian Skorobogatow <christiansko@icloud.com>
2022-09-24 10:07:46 +0200

    feat(): setup coredata with entity + initial setup for coredata service

--- CryptoApp/CryptoAppApp.swift
+++ CryptoApp/CryptoAppApp.swift
@@ -15,7 +15,6 @@ struct CryptoAppApp: App {
     init() {
         UINavigationBar.appearance().largeTitleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor : UIColor(Color.theme.accent)]
-        
     }
     
     var body: some Scene {
