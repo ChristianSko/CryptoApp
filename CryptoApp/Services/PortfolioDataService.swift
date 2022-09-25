bcfe0674fece44f46033c82f511a73b21751d970
Christian Skorobogatow <christiansko@icloud.com>
2022-09-25 17:27:06 +0200

    feat(): finish building coreData service

--- CryptoApp/Services/PortfolioDataService.swift
+++ CryptoApp/Services/PortfolioDataService.swift
@@ -16,15 +16,32 @@ class PortfolioDataService {
     
     @Published var savedEntities: [PortfolioEntity] = []
     
-    
     init() {
         container = NSPersistentContainer(name: containerName)
         container.loadPersistentStores { ( _, error) in
             if let error = error {
                 print("Error loading Core Data : \(error)")
             }
+            self.getPortfolio()
         }
     }
+    
+    // MARK: PUBLIC
+    
+    func updatePortfolio(coin: CoinModel, amount: Double) {
+    
+        if let entity = savedEntities.first(where: { $0.coinID == coin.id }) {
+            if amount > 0 {
+                update(entity: entity, amount: amount)
+            } else {
+                delete(entity: entity)
+            }
+        } else {
+            add(coin: coin, amount: amount)
+        }
+    }
+    
+    // MARK: PRIVATE
     
     private func getPortfolio() {
         let request = NSFetchRequest<PortfolioEntity>(entityName: entityName)
@@ -36,6 +53,34 @@ class PortfolioDataService {
         }
     }
     
+    private func add(coin: CoinModel, amount: Double) {
+        let entity = PortfolioEntity(context: container.viewContext)
+        entity.coinID = coin.id
+        entity.amount = amount
+        applyChanges()
+    }
     
+    private func update(entity: PortfolioEntity, amount: Double) {
+        entity.amount = amount
+        applyChanges()
+    }
+    
+    private func delete(entity: PortfolioEntity) {
+        container.viewContext.delete(entity)
+        applyChanges()
+    }
+    
+    private func save() {
+        do {
+            try container.viewContext.save()
+        } catch let error {
+            print("Error saving to Core Data \(error)")
+        }
+    }
+    
+    private func applyChanges() {
+        save()
+        getPortfolio()
+    }
     
 }
